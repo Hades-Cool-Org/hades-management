95a5244361acbc9895de88a17f3ab635721b75ab
Caio Sousa <caiofmsousa@gmail.com>
2023-02-23 07:15:34 -0300

    Add Header and Footer components, also added prettier to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "../styles/layout.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,39 @@
-import { Button, FormControl, TextField } from "@mui/material"
-import styles from '@/styles/AuthenticationPage.module.css'
-import Router from "next/router"
+import { Button, FormControl, TextField } from "@mui/material";
+import styles from "@/styles/AuthenticationPage.module.css";
+import Router from "next/router";
+import Footer from "@/components/Footer";
+import { ReactElement } from "react";
 
-const AuthenticationPage: React.FC = () => {
-    return (
+const Authentication = () => {
+  return (
     <>
-        <main className={styles.main}>
-            <form>
-                    <TextField id="standard-basic" label="Login" variant="standard"/>
-                    <TextField id="standard-basic" label="Password" variant="standard" type="password"/>     
-                    <Button variant="contained" onClick={() => Router.push('./configuration')}>Submit</Button>           
-            </form>
-        </main>
+      <main className={styles.main}>
+        <form>
+          <TextField id="standard-basic" label="Login" variant="standard" />
+          <TextField
+            id="standard-basic"
+            label="Password"
+            variant="standard"
+            type="password"
+          />
+          <Button
+            variant="contained"
+            onClick={() => Router.push("./configuration")}
+          >
+            Submit
+          </Button>
+        </form>
+      </main>
     </>
-    )
+  );
+};
+export default Authentication;
+
+Authentication.getLayout = function PageLayout(page: ReactElement){
+        return (
+            <>
+                {page}
+                <Footer/>
+            </>
+        )
 }
-export default AuthenticationPage
