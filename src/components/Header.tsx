5fc9e1fe9743162b2c0d0ba964fd9f01938473fb
Caio Sousa <caiofmsousa@gmail.com>
2023-02-23 08:29:10 -0300

    Add Menu icon and label to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,21 @@
+import { Menu } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import Router from "next/router";
+
 const Header = () => {
-  return <div className="layout-header">Header</div>;
-}
+  return (
+    <>
+      <div className="layout-header">
+        <IconButton
+          aria-label="config-button"
+          onClick={() => Router.push("./configuration")}
+        >
+          <Menu fontSize="large" style={{ fill: "white" }} />
+        </IconButton>
+        <span className="title-span">Perfil - Comprador</span>
+      </div>
+    </>
+  );
+};
 
 export default Header;
