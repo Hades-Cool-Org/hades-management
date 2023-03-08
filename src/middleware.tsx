031e22a1e49b57f42009559d5fd4f9cab66e3fa3
Caio Sousa <caiofmsousa@gmail.com>
2023-03-08 16:02:04 -0300

    Move redirect logic to middleware that will handle it

---
+++ src/middleware.tsx
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { origin } = request.nextUrl;
+  const verify = request.cookies.get("loggedIn");
+
+  if (!verify) {
+    return NextResponse.redirect(`${origin}/login`);
+  }
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|favicon.ico|login).*)"],
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,10 @@ import Head from "next/head";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Root.module.css";
 import React from "react";
-import Context from "@/components/Context";
-import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Root() {
-  const value = React.useContext(Context);
-  const router = useRouter();
-
-  React.useEffect(() => {
-    if (!value.state.user) {
-      router.push("/login");
-    }
-  }, []);
-
   return (
     <>
       <Head>
