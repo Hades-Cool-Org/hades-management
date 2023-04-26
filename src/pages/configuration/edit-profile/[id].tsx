06c5860dc2f3c581afc75024025a9994c27d9722
Caio Sousa <caiofmsousa@gmail.com>
2023-04-26 09:58:34 -0300

    Update edit user page to use api hooks

--- src/pages/configuration/edit-profile/[id].tsx
+++ src/pages/configuration/edit-profile/[id].tsx
@@ -1,62 +1,72 @@
-import * as React from "react";
-import axios from "axios";
-
+import React, { useState } from "react";
 import styles from "@/styles/AuthenticationPage.module.css";
 import { Autocomplete, Button, TextField } from "@mui/material";
 import TextFieldStandard from "@/components/TextField";
+import useRequest from "@/hooks/useRequest";
 
 export default function EditProfile() {
   interface Role {
     name: string;
   }
 
-  const [name, setName] = React.useState<string>("");
-  const [email, setEmail] = React.useState<string>("");
-  const [phone, setPhone] = React.useState<string>("");
-  const [password, setPassword] = React.useState<string>("");
-  const [roles, setRoles] = React.useState<Role[]>([]);
+  const { put } = useRequest();
+
+  const [body, setBody] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const handleChange = (name: string, value: string) => {
+    setBody((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
-    const body = {
-      name,
-      email,
-      phone,
-      password,
-      roles
-    };
-    const data = await axios.put("http://localhost:3333/v1/users/1", body);
-    console.log(data);
+    put("http://localhost:3333/v1/users/1", body);
+    console.log(body);
   };
 
   return (
     <main className={styles.main}>
       <form>
-        <TextFieldStandard label="Username" handleChange={setName} />
-        <TextFieldStandard label="user@address.com" handleChange={setEmail} />
-        <TextFieldStandard label="(19)00000-0000" handleChange={setPhone} />
+        <TextFieldStandard
+          label="Username"
+          handleChange={handleChange}
+          fieldName={"name"}
+        />
+        <TextFieldStandard
+          label="user@address.com"
+          handleChange={handleChange}
+          fieldName={"email"}
+        />
+        <TextFieldStandard
+          label="(19)00000-0000"
+          handleChange={handleChange}
+          fieldName={"phone"}
+        />
         <TextField
           id="standard-basic"
           label="Password"
           variant="standard"
           type="password"
           onChange={(e) => {
-            setPassword(e.target.value);
+            handleChange("password", e.target.value);
           }}
         />
         <Autocomplete
-        multiple
-        id="tags-standard"
-        options={[{name:'admin'}, {name:'castor'}]}
-        getOptionLabel={(option) => option.name}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="standard"
-            label="Roles"
-          />
-        )}
-      />
+          multiple
+          id="tags-standard"
+          options={[{ name: "admin" }, { name: "castor" }]}
+          getOptionLabel={(option) => option.name}
+          renderInput={(params) => (
+            <TextField {...params} variant="standard" label="Roles" />
+          )}
+        />
         <Button variant="contained" onClick={handleSubmit}>
           Salvar
         </Button>
