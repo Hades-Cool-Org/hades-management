fdcaa1886ebebdc0ecc8af3707934894e5adbfc5
Caio Sousa <caiofmsousa@gmail.com>
2023-05-03 07:35:28 -0300

    Add flow to start a session

--- src/pages/motorista/index.tsx
+++ src/pages/motorista/index.tsx
@@ -1,15 +1,91 @@
 import Head from "next/head";
 import styles from "@/styles/Root.module.css";
 
-const Motorista = () => {
+import useFetch from "@/hooks/useFetch";
+import {
+  Button,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import useRequest from "@/hooks/useRequest";
+
+const Motorista = ({ user }) => {
+  const [vehicleId, setVehicleId] = useState<string>("");
+
+  const { data, loading, error } = useFetch(
+    "http://localhost:3333/v1/deliveries/vehicles"
+  );
+
+  const request = useRequest();
+
+  const userData = JSON.parse(user);
+
+  const handleVehicleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newValue: string
+  ) => {
+    setVehicleId(newValue);
+  };
+
+  const handleClick = () => {
+    const body = { user: { id: userData.user_id }, vehicle: { id: vehicleId } };
+    request.post("http://localhost:3333/v1/deliveries/sessions", body);
+  };
+
+  useEffect(() => {
+    if (request.success) {
+      console.log("success");
+    }
+  }, [request.success]);
+
+  if (loading) return <h1>Loading</h1>;
+  if (error) console.log(error);
   return (
     <>
       <Head>
         <title>Motorista</title>
       </Head>
-      <main className={styles.main}>Motorista</main>
+      {data && (
+        <main className={styles.main}>
+          <Typography>{userData.name}</Typography>
+
+          <ToggleButtonGroup
+            color="primary"
+            exclusive
+            aria-label="Platform"
+            value={vehicleId}
+            onChange={handleVehicleChange}
+            orientation="vertical"
+          >
+            {
+              // @ts-ignore: Object is possibly 'null'
+              data?.vehicles.map((vehicle: Product, index) => {
+                return (
+                  <ToggleButton value={vehicle.id}>
+                    {
+                      <>
+                        <h1>{vehicle.name}</h1>
+                        <h2>{vehicle.type}</h2>
+                      </>
+                    }
+                  </ToggleButton>
+                );
+              })
+            }
+          </ToggleButtonGroup>
+          <Button onClick={handleClick}>Iniciar Sessão</Button>
+        </main>
+      )}
     </>
   );
 };
+
+export function getServerSideProps({ req, res }) {
+  return {
+    props: { user: req.cookies.user },
+  };
+}
 
 export default Motorista;
