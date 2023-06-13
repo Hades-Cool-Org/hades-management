b1629c5efc42b54f9970fe200d11c0250b0c74e1
Caio Sousa <caiofmsousa@gmail.com>
2023-06-13 03:04:14 -0300

    Add page to check occurrences

--- src/pages/acompanhamento/index.tsx
+++ src/pages/acompanhamento/index.tsx
@@ -1,6 +1,45 @@
+import BaseCard from "@/components/Card/BaseCard";
+import useFetch from "@/hooks/useFetch";
+import { Box, Divider, Typography } from "@mui/material";
 import React from "react";
 
 export default function Acompanhamento() {
-  // const { data } = useFetch(`http://localhost:3333/v1/conference/occurrences?order_id=${}`);
-  return <main className="main">Acompanhamento</main>;
+  const { data } = useFetch(`http://localhost:3333/v1/conference/occurrences`);
+  const { data: storeData } = useFetch(`http://localhost:3333/v1/store`);
+
+  return (
+    <main className="main">
+      <Box className="main-content">
+        {data &&
+          storeData &&
+          data?.occurrences?.map((occurrence, index) => {
+            return (
+              <BaseCard key={index}>
+                <Box className="main-content">
+                  <Typography variant="h5">
+                    {
+                      storeData.stores.find((store) => {
+                        return store.id === occurrence.store_id;
+                      }).name
+                    }
+                  </Typography>
+                  {occurrence.items.map((item, index) => {
+                    return (
+                      <>
+                        <Divider flexItem />
+                        <span>
+                          Produto: {item.name}, Pêndencia: {item.type},
+                          Quantidade: {item.quantity} {/* eslint-disable-line*/}
+                        </span>
+                        <br />
+                      </>
+                    );
+                  })}
+                </Box>
+              </BaseCard>
+            );
+          })}
+      </Box>
+    </main>
+  );
 }
